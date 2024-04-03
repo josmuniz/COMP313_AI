d8b9ce5053f7f0e512efab1fbf579291bd5bd683
josmuniz <jmuniz2011@gmail.com>
2024-04-03 10:31:59 -0400

    Final version.
    Include : Predict risk consume and predict risk Heroin Consume

--- APP_comp313_flask_v0.py
+++ APP_comp313_flask_v0.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import joblib
+from keras.models import load_model
 import pandas as pd
 import numpy as np
 import sys
@@ -9,17 +10,26 @@ import traceback
 
 
 # Folder and models
-project_folder = r'/Users/josemuniz/Desktop/COMP313_AI_PRY/best_result'
+project_folder = r'/Users/josemuniz/Desktop/COMP313_AI/best_result'
 
 # Load the trained models and the scaler
 lr_model = joblib.load(f'{project_folder}/LR_model_drug_use.pkl')
+lr_model_h = joblib.load(f'{project_folder}/LR_model_heroin.pkl')
 rf_model = joblib.load(f'{project_folder}/RF_model_drug_use.pkl')
+dl_model = load_model(f'{project_folder}/ANN_model_drug_use.h5')
 scaler   = joblib.load(f'{project_folder}/scaler_drug_use.pkl')
 
 # Define feature names as expected by the model
 feature_names = ['Nscore', 'Escore', 'Oscore', 'Ascore', 'Cscore', 'Impulsive', 'SS', 
                  'Alcohol', 'Amphet', 'Amyl', 'Benzos', 'Cannabis', 'Coke', 'Crack', 'Ecstasy',
                  'Heroin', 'Ketamine', 'Legalh', 'LSD', 'Meth', 'Mushrooms', 'Nicotine', 'VSA']
+
+# Define feature names as expected by the model
+feature_names_h = ['Nscore', 'Escore', 'Oscore', 'Ascore', 'Cscore', 'Impulsive', 'SS', 
+                 'Alcohol', 'Amphet', 'Amyl', 'Benzos', 'Cannabis', 'Coke', 'Crack', 'Ecstasy',
+                 'Ketamine', 'Legalh', 'LSD', 'Meth', 'Mushrooms', 'Nicotine', 'VSA']
+
+models=['Heroin']
 
 app = Flask(__name__)
 CORS(app)
@@ -31,9 +41,10 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        data = request.get_json(force=True)
+        data = request.get_json(force=True)        
         print('Received data:', data)
-        print(f'ruta: {lr_model}')
+        #print(f'ruta: {lr_model}')
+        
         # Extract the features from the data
         features = np.array([[data.get(feature, 0) for feature in feature_names]])
         print (f'features: {features}')
@@ -42,19 +53,32 @@ def predict():
         print (f'features_scaled: {features_scaled}')
         # Generate predictions using the trained models
 
-        print(f'lr_result: {lr_model.predict(features_scaled)[0]}')
-        print(f'lr_result: {lr_model.predict(features_scaled)}')
-
+        # Extract the features from the data
+        features_h = np.array([[data.get(feature, 0) for feature in feature_names_h]])
+        print (f'features_h: {features_h}')
+        # Scale the features using the loaded scaler
+        features_h_scaled = np.array(features_h).reshape(1, -1)
+        print (f'features_h_scaled: {features_h_scaled}')
+        # Generate predictions using the trained models
+        lr_result_h = "Likely" if lr_model_h.predict(features_h_scaled)[0]> 0.5 else "Unlikely"
+        print (f'LR_h is {lr_result_h}')
+        
+        
         lr_result = "Likely" if lr_model.predict(features_scaled)[0]> 0.5 else "Unlikely"
         print (f'LR is {lr_result}')
         rf_result = "Likely" if rf_model.predict(features_scaled)[0]> 0.5 else "Unlikely"
         print (f'RF is {rf_result}')
-        # Return the prediction results as JSON
-        # Prepare the response
+        dl_result = "Likely" if dl_model.predict(features_scaled)[0]> 0.5 else "Unlikely"
+        print (f'DNN is {dl_result}')
+                
+                     
         response = {
-            'prediction1': lr_result,
-            'prediction2': rf_result
-        }
+                'prediction1': lr_result,
+                'prediction2': rf_result,
+                'prediction3': dl_result,
+                'prediction4': lr_result_h,
+            }
+        
         
         return jsonify(response)
 
@@ -68,6 +92,6 @@ if __name__ == '__main__':
     try:
         port = int(sys.argv[1])
     except:
-        port = 12412
+        port = 1200
 
     app.run(port=port, debug=True)
